5ce55129a475fbc58b621a05fd98fa246eb34eb8
arijec <adis.tulusal@gmail.com>
2019-04-02 11:21:12 +0200

    Update

--- interpret.py
+++ interpret.py
@@ -6,6 +6,12 @@ src_file = ""
 input_file = ""
 stats_file = ""
 
+instruction_counter = 1
+
+
+#################
+# checking correctness of arguments
+#################
 def check_arguments2(argv, arguments_dic):
     global src_file
     global input_file
@@ -35,7 +41,7 @@ def check_arguments2(argv, arguments_dic):
             
         elif "--input" in i and not arguments_dic["input"]:
             if "--input=" == i[:8:]:
-                arguments_dic["source"] = True
+                arguments_dic["input"] = True
                 input_file = i[8::]
                 
                 try:
@@ -124,26 +130,184 @@ def read_from_stdin():
         if not line:
             break
     
+    print(string)
     return string
+
+
+#################
+# handling instructions
+#################
+def get_highest_order_number(program):
+    top = 0
+
+    for instruction in program:
+        if int(instruction.attrib["order"]) > top:
+            top = int(instruction.attrib["order"])
+
+    return top
+
+
+def get_program_dictionary(program):
+    dic = {}
+
+    for instruction in program:
+        dic.update({int(instruction.attrib["order"]): instruction})
+
+    return dic
+
+
+def instruction_switch(instruction):
+    opcode = instruction.attrib["opcode"].upper()
+
+
+    ###########
+    # 6.4.1 ramce, volanie funkcii
+    if opcode == "MOVE":
+        handle_move()
+    elif opcode == "CREATEFRAME":
+        handle_createframe()
+    elif opcode == "PUSHFRAME":
+        handle_pushframe()
+    elif opcode == "POPFRAME":
+        handle_popframe()
+    elif opcode == "DEFVAR":
+        handle_defvar()
+    elif opcode == "CALL":
+        handle_call()
+    elif opcode == "RETURN":
+        handle_return()
+
+    ###########
+    # 6.4.2 datovy zasobnik
+    elif opcode == "PUSHS":
+        handle_pushs()
+    elif opcode == "POPS":
+        handle_pops()
+        
+    ###########
+    # 6.4.3 aritmeticke, relacne, booleovske a konverzne instrukcie
+    elif opcode == "ADD":
+        pass
+    elif opcode == "SUB":
+        pass
+    elif opcode == "MUL":
+        pass
+    elif opcode == "IDIV":
+        pass
+    elif opcode == "LT":
+        pass
+    elif opcode == "GT":
+        pass
+    elif opcode == "EQ":
+        pass
+    elif opcode == "AND":
+        pass
+    elif opcode == "OR":
+        pass
+    elif opcode == "NOT":
+        pass
+    elif opcode == "INT2CHAR":
+        pass
+    elif opcode == "STR2INT":
+        pass
+
+    ###########
+    # 6.4.4 vytupne a vystupne instrukcie
+    elif opcode == "READ":    
+        pass
+    elif opcode == "WRITE":
+        pass
+
+    ###########
+    # 6.4.5 praca s retazcami
+    elif opcode == "CONCAT":
+        pass
+    elif opcode == "STRLEN":
+        pass
+    elif opcode == "GETCHAR":
+        pass
+
+    ###########
+    # 6.4.6 praca s typmi
+    elif opcode == "TYPE":
+        pass
+
+    ###########
+    # 6.4.7 riadenie toku programu
+    elif opcode == "LABEL":
+        pass
+    elif opcode == "JUMP":
+        pass
+    elif opcode == "JUMPIFEQ":
+        pass
+    elif opcode == "JUMPIFNEQ":
+        pass
+    elif opcode == "EXIT":
+        pass
+        
+    ###########
+    # 6.4.8 ladiace instrukcie
+    elif opcode == "DPRINT":
+        pass
+    elif opcode == "BREAK":
+        pass
+
+    else
+        exit(32)
+
+
+
+
+
+def handle_instructions(program):
+    global instruction_counter
+    
+    top_order = get_highest_order_number(program)
+
+    program = get_program_dictionary(program) 
+
+    
+
+    print(program[2].attrib)
+
+    while instruction_counter <= top_order:
+
+        instruction_switch(program[instruction_counter])
+
+        instruction_counter += 1
+
+
+
+
+
+
+
 
 
 ######### MAIN #########
 
 
 arguments_dic = check_arguments(argv)
-
+print(arguments_dic)
 
 if not arguments_dic["source"] or not arguments_dic["input"]:
     if not arguments_dic["source"]:
         src_file = read_from_stdin()
+
     if not arguments_dic["input"]:
         input_file = read_from_stdin()
 
-
 program = ET.fromstring(src_file)
 
+
+handle_instructions(program)
+
+
 for instruction in program:
+    print(instruction.tag)
     print(instruction.attrib["order"])
+    print(instruction.attrib["opcode"])
+    print(instruction.text)
     for argument in instruction:
         print(argument.tag)
         print(argument.attrib)
